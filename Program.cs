b94a23e342e4738540cece2f18bda55e578618d8
nashEm8 <emanuel82016@outlook.com>
2024-08-26 23:36:49 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Projeto_Web_Lh_Pets_versão_1;
+
+namespace Projeto_Web_Lh_Pets_Alunos;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web - LH Pets");
+
+        app.UseStaticFiles();
+
+        app.MapGet("/index", (HttpContext contexto) => {
+                contexto.Response.Redirect("index.html", false);
+        });
+
+        Banco dba = new Banco();
+
+        app.MapGet("/listaClientes", (HttpContext contexto) => {
+            
+            contexto.Response.WriteAsync(dba.GetListaString());
+
+        });
+
+
+        app.Run();
+    }
+}
